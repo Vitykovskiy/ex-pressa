77e677f5d05e07f0a97c69a50fd98e5bacc0def6
Vitykovskiy <vitykovskiy@gmail.com>
2026-03-10 16:23:13 +0300

    fix: run prod deploy with bash
    Version: 0.0.1+20260310.132313

--- src/generated/app-version.ts
+++ src/generated/app-version.ts
@@ -1,7 +1,7 @@
-export const APP_VERSION = '0.0.1+20260310.131441';
+export const APP_VERSION = '0.0.1+20260310.132313';
 export const APP_VERSION_INFO = {
   appName: 'ex-pressa',
   packageVersion: '0.0.1',
   version: APP_VERSION,
-  generatedAt: '2026-03-10T13:14:41.414Z',
+  generatedAt: '2026-03-10T13:23:13.414Z',
 } as const;
